e06e4e3dc16a9f510a4e4cba39a30b72b1e8accc
afanasiy <afanasiy.2006@mail.ru>
2026-08-18 22:34:05 +0600

    18.08.2026

--- zapret/def-cfg.sh
+++ zapret/def-cfg.sh
@@ -369,6 +369,15 @@ function set_cfg_nfqws_strat
 				--dpi-desync-repeats=8
 				--dpi-desync-split-seqovl-pattern=/opt/zapret/files/fake/tls_clienthello_www_google_com.bin
 				--dpi-desync-fake-tls=/opt/zapret/files/fake/tls_clienthello_www_google_com.bin
+
+				--new
+				#udp443
+				--filter-udp=443
+				--hostlist=/opt/zapret/ipset/zapret-hosts-google.txt
+				--dpi-desync=fake
+				--dpi-desync-repeats=11
+				--dpi-desync-fake-quic=/opt/zapret/files/fake/quic_initial_www_google_com.bin
+				
 				--new
 				--filter-tcp=80,443
 				--hostlist-exclude=/opt/zapret/ipset/zapret-hosts-user-exclude.txt
